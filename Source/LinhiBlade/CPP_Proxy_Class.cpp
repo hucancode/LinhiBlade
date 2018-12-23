77fbf268ba38c65ed70a3288f22f84c7cb4495c8
hucancode <hucancode@gmail.com>
2018-12-24 00:07:58 +0700

    add c++ build

---
+++ Source/LinhiBlade/CPP_Proxy_Class.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CPP_Proxy_Class.h"
+
+CPP_Proxy_Class::CPP_Proxy_Class()
+{
+}
+
+CPP_Proxy_Class::~CPP_Proxy_Class()
+{
+}
---
+++ Source/LinhiBlade/CPP_Proxy_Class.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class LINHIBLADE_API CPP_Proxy_Class
+{
+public:
+	CPP_Proxy_Class();
+	~CPP_Proxy_Class();
+};
